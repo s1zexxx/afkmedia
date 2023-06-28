39b2e43b03431e865c47603d4879c4eb6dd8bf7a
s1zexxx <135944334+s1zexxx@users.noreply.github.com>
2023-06-28 11:12:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import config
+
 # Get it from my.telegram.org
 API_ID = int(getenv("23786230"))
 API_HASH = getenv("c3a267db65847bfff3177db9084506ef")
