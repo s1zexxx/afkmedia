44b619011dbadbd01c87551d3cc0475e3f528a39
s1zexxx <135944334+s1zexxx@users.noreply.github.com>
2023-06-28 10:15:35 +0500

    Add files via upload

---
+++ s1zex/database/__init__.py
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021-2022 by Teams1zex@Github, < https://github.com/Teams1zex >.
+#
+# This file is part of < https://github.com/Teams1zex/s1zexAFKBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Teams1zex/s1zexAFKBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+
+from .chats import *
+from .users import *
+from .cleanmode import *
